07fbd6df8a1a9bedecba0738578583e638b551de
Wanjiku Kimani <wanjikukimani24@gmail.com>
2022-11-21 19:37:28 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+//multiplication
+let num1 = 2;
+let num2 = 31
+
+let multiply = num1 * num2;
+//to generate an a random number we use Math.random()
+//it generates a number between 0-0.9999...
+//to make it greater than 0 we multiply by 10
+//to check if it's an integer we use Number.isInteger
+
+let random = Number.isInteger(Math.random() *10) + 1;
+ 
+// using the modulus operator to check remainder
+
+const num3 = 34;
+const num4 = 5;
+let mod = 34%5;
+mod;
+
+let max = Math.max(1,5, 7,10,20);
+max;
